b0250ef5672231d06e523bf2f8f07450c720c5bb
Vitaly Kurennov <vkurennov@gmail.com>
2015-11-26 19:28:13 +0300

    Helper link_with_icon

--- app/helpers/application_helper.rb
+++ app/helpers/application_helper.rb
@@ -1,2 +1,8 @@
 module ApplicationHelper
+  def link_with_icon(icon, path, options = {})
+    title = options.delete(:title)
+    link_to path, options do
+      content_tag(:i, title, class: "fa fa-#{icon}")
+    end
+  end
 end
